37e064878e488f5148cba02bae0ba49f1691240d
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-27 08:57:56 -0600

    Initial Commit

---
+++ EasyScreenshoot/EasyScreenshootApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyScreenshootApp.swift
+//  EasyScreenshoot
+//
+//  Created by Brenda Saavedra  on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct EasyScreenshootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
